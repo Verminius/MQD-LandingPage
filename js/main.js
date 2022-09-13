e3c889370268d59c2d1f6528f453368168fb615d
Alexander Patiño <alpatino.ortiz@gmail.com>
2022-09-12 21:58:55 -0500

    js cards implemented

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
+// Menu desplegable
+
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
-menuBtn.addEventListener("click", function () {
-  menu.classList.toggle("desplegado");
+menuBtn.addEventListener("click", function() {
+    menu.classList.toggle("desplegado");
 });
+
+// Modal
+
+const modal = document.getElementById("cont-modal");
+const disenoBtn = document.getElementById("diseno-grafico");
+const digitalBtn = document.getElementById("digitales");
+const merchBtn = document.getElementById("merchandising");
+const bigFormatBtn = document.getElementById("gran-formato");
+const didacticoBtn = document.getElementById("didactico");
+const closeModal = document.querySelector(".btn-sec");
+
+closeModal.addEventListener("click", () => {
+    modal.classList.remove("show-modal");
+})
+
+disenoBtn.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+})
+
+digitalBtn.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+})
+
+merchBtn.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+})
+
+didacticoBtn.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+})
+
+bigFormatBtn.addEventListener("click", () => {
+    modal.classList.add("show-modal");
+})
